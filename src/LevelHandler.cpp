8cb0c5de17a325cfb35f0f9c67e48dea180d8824
Callum Darling <callum.darling1@gmail.com>
2020-03-27 17:51:02 +0000

    level creation functional

--- src/LevelHandler.cpp
+++ src/LevelHandler.cpp
@@ -4,11 +4,53 @@ LevelHandler::LevelHandler(){
     //
 }
 
-int LevelHandler::loadLevel(std::string fileName){
+int LevelHandler::loadLevel(std::string fileName,
+                            ResourceHandler<sf::Texture, Textures::ID>& textures
+                            ,std::vector<Entity>& entList){
+
     std::ifstream file(fileName);
     if(file){
-        for( std::string line; std::getline( file, line ); ){
-            std::cout << line << std::endl;
+        int stage = 0;
+       for( std::string line; std::getline( file, line ); ){
+           
+           std::vector<std::string> strings;
+            // std::cout << stage << line << std::endl;
+            std::stringstream sS(line);
+            while(sS.good()){
+                std::string substr;
+                std::getline(sS, substr, ',');
+                strings.push_back(substr);
+                std::cout <<  substr << std::endl;
+            }
+
+            if( line == "###"){
+                stage++;
+            }else if(stage == 0){
+                try{
+                    Entity e(textures.get(Textures::Block), std::stoi(strings[0])*20.f, std::stoi(strings[1])*20.f, false);
+                    entList.push_back(e);
+                }catch(const std::exception& ex){
+                    std::cout << "levelfail" << std::endl;
+                }
+            }else if(stage == 1){
+                 try{
+                    Entity e(textures.get(Textures::Boss), std::stoi(strings[0])*20.f, std::stoi(strings[1])*20.f, true);
+                    entList.push_back(e);
+                }catch(const std::exception& ex){
+                    std::cout << "levelfail" << std::endl;
+                }
+
+            }else if(stage == 2){
+                 try{
+                    Entity e(textures.get(Textures::Henchman), std::stoi(strings[0])*20.f, std::stoi(strings[1])*20.f, true);
+                    entList.push_back(e);
+                }catch(const std::exception& ex){
+                    std::cout << "levelfail" << std::endl;
+                }
+
+            }else if(stage == 3){
+
+            }
         }     
 
         return 1;
