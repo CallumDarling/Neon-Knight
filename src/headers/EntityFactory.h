af0efb58d23700ddae0ba29d6a274881ef10ba9d
Callum Darling <callum.darling1@gmail.com>
2020-05-11 17:47:07 +0100

    moved functionality to cpp file

--- src/headers/EntityFactory.h
+++ src/headers/EntityFactory.h
@@ -2,26 +2,10 @@
 #include "../entt/entt.hpp"
 #include "ResourceHandler.h"
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
 class EntityFactory{
     public:
-        entt::entity createPlayer(entt::registry &registry, std::string textFilename){
-            //declare variables needed for Draw component
-            ResourceHandler<sf::Texture, Textures::ID> text;
-            try{
-                text.load(Textures::Player, "media/textures/att2.png");
-            }catch(const std::exception& e){
-            }
-            sf::Vector2f loc;
-            loc.x = 0.0;
-            loc.y = 0.0;
-            sf::Sprite sprit;
-            sprit.setTexture(text.get(Textures::Player));
-            sprit.setPosition(loc);
-            
-            //creates entity and adds components
-            entt::entity e = registry.create();
-            registry.emplace<Draw>(e, text.get(Textures::Player),sprit,true,loc);
-            return e;
-        }
+        entt::entity createPlayer(entt::registry &registry, std::string textFilename, ResourceHandler<sf::Texture, Textures::ID> &text);
+
 };
