6bee60da026446855d60e12daa9a606588301488
Callum Darling <callum.darling1@gmail.com>
2020-05-12 17:51:05 +0100

    moved entity values to seperate file for ease

---
+++ src/headers/EntityConfig.h
@@ -0,0 +1,37 @@
+const int bruteAttack = 20;
+const int bruteRange = 150;
+const int bruteVision = 500;
+const float bruteIdleTime = 15.f;
+const float bruteTransitionTime = 0.f;
+const int bruteMaxHealth = 50;
+
+
+const int henchMelee = 20;
+const int henchMeleeRange = 150;
+const int henchVision = 500;
+const std::string henchProj = "bullet";
+const int henchProjRange = 20;
+const int henchProjDamage = 20;
+const float henchIdleTime = 15.f;
+const float henchTransitionTime = 0.f;
+const int henchMaxHealth = 75;
+
+
+const int gunVision = 500;
+const std::string gunProj = "bullet";
+const int gunProjRange = 20;
+const int gunProjDamage = 20;
+const float gunIdleTime = 15.f;
+const float gunTransitionTime = 0.f;
+const int gunMaxHealth = 20;
+
+
+const int bossMelee = 20;
+const int bossMeleeRange = 150;
+const int bossVision = 500;
+const std::string bossProj = "bullet";
+const int bossProjRange = 20;
+const int bossProjDamage = 20;
+const float bossIdleTime = 15.f;
+const float bossTransitionTime = 0.f;
+const int bossMaxHealth = 150;
