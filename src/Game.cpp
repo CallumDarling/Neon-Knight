d47b902160f704794b8252fad8726f33ad6591fa
Callum Darling <callum.darling1@gmail.com>
2020-05-14 20:59:43 +0100

    completed collision system 3rd time lucky

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,15 +1,14 @@
 #include "headers/Game.h"
 
-Game::Game(): 
-    worldView(sf::FloatRect(32.f, 32.f, 768.f, 432.f)),
-    mWindow(sf::VideoMode(1600,900), "SFML Application", sf::Style::Fullscreen){
-        mWindow.setKeyRepeatEnabled(true);
-        mWindow.setVerticalSyncEnabled(true);
-        // mWindow.setFramerateLimit(60);
-    }
+Game::Game() : worldView(sf::FloatRect(32.f, 32.f, 768.f, 432.f)),
+               mWindow(sf::VideoMode(1600, 900), "SFML Application", sf::Style::Fullscreen) {
+    mWindow.setKeyRepeatEnabled(true);
+    mWindow.setVerticalSyncEnabled(true);
+    // mWindow.setFramerateLimit(60);
+}
 
-bool Game::initTextures(std::vector<Entity>& eList){
-    try{
+bool Game::initTextures(std::vector<Entity> &eList) {
+    try {
         textures.load(Textures::Player, "media/textures/att2.png");
         textures.load(Textures::Henchman, "media/textures/hench1.png");
         textures.load(Textures::Boss, "media/textures/boss1.png");
@@ -20,128 +19,122 @@ bool Game::initTextures(std::vector<Entity>& eList){
         textures.load(Textures::Platform, "media/textures/platform.png");
         textures.load(Textures::Door, "media/textures/door.png");
         textures.load(Textures::Bullet, "media/textures/bullet.png");
-    }catch(const std::exception& e){
+    } catch (const std::exception &e) {
         return 0;
     }
     Entity e(textures.get(Textures::Player), 200.f, 200.f, true);
     eList.push_back(e);
     LevelHandler lvlH;
-    lvlH.loadLevel("levels/lvl1", textures , entList, registry);
+    lvlH.loadLevel("levels/lvl1", textures, entList, registry);
     return 1;
 }
 
-void Game::processEvents(){
+void Game::processEvents() {
     sf::Event event;
-    while(mWindow.pollEvent(event)){
-        switch(event.type){
-            case sf::Event::KeyPressed:
-                handlePlayerInput(event.key.code, true);
-                break;
-            case sf::Event::KeyReleased:
-                handlePlayerInput(event.key.code, false);
-                break;
-            case sf::Event::Closed:
-                mWindow.close();
-                break;
-
+    while (mWindow.pollEvent(event)) {
+        switch (event.type) {
+        case sf::Event::KeyPressed:
+            handlePlayerInput(event.key.code, true);
+            break;
+        case sf::Event::KeyReleased:
+            handlePlayerInput(event.key.code, false);
+            break;
+        case sf::Event::Closed:
+            mWindow.close();
+            break;
         }
     }
 }
 
-void Game::handlePlayerInput(sf::Keyboard::Key key, bool isPressed){
-    if( key == sf::Keyboard::Up){
+void Game::handlePlayerInput(sf::Keyboard::Key key, bool isPressed) {
+    if (key == sf::Keyboard::Up) {
         misMovingUp = isPressed;
-    }else if( key == sf::Keyboard::Down){
+    } else if (key == sf::Keyboard::Down) {
         misMovingDown = isPressed;
-    }else if( key == sf::Keyboard::Left){
+    } else if (key == sf::Keyboard::Left) {
         misMovingLeft = isPressed;
-    }else if( key == sf::Keyboard::Right){
+    } else if (key == sf::Keyboard::Right) {
         misMovingRight = isPressed;
-    }else if( key == sf::Keyboard::SemiColon){
+    } else if (key == sf::Keyboard::SemiColon) {
         Entity player = entList[0];
         entList.clear();
         entList.push_back(player);
         LevelHandler lvlH;
-        lvlH.loadLevel("levels/lvl1", textures , entList, registry);
-    }else if( key == sf::Keyboard::Space){
-        if(playerOnFloor){
+        lvlH.loadLevel("levels/lvl1", textures, entList, registry);
+    } else if (key == sf::Keyboard::Space) {
+        if (playerOnFloor) {
             playerJump = 1050;
             playerOnFloor = false;
         }
     }
-
 }
 
-void Game::initWindow(){
+void Game::initWindow() {
     // mWindow.setView(worldView);
-    sf::Vector2u wSize  = mWindow.getSize();
+    sf::Vector2u wSize = mWindow.getSize();
     sf::Vector2f vSize = worldView.getSize();
     float sizeX = 1, sizeY = 1;
-    float posX =0, posY = 0;
-    float wRat = (float) wSize.x / (float) wSize.y;
+    float posX = 0, posY = 0;
+    float wRat = (float)wSize.x / (float)wSize.y;
     float vRat = vSize.x / vSize.y;
-    
+
     std::cout << wSize.x << "w" << wSize.y << std::endl;
     std::cout << vSize.x << "v" << vSize.y << std::endl;
     // bool barsTop = true;
     std::cout << "wRat " << wRat << std::endl;
     std::cout << "vRat " << vRat << std::endl;
-    if(wRat==vRat){
-    }else if(wRat < vRat){
+    if (wRat == vRat) {
+    } else if (wRat < vRat) {
         //bars at the topfalsel;
         sizeY = wRat / vRat;
-        posY = (1-sizeY) / 2.f;
+        posY = (1 - sizeY) / 2.f;
 
-    }else{
+    } else {
         std::cout << "barSide" << std::endl;
         sizeX = vRat / wRat;
-        posX = (1-sizeX) / 2.f; 
+        posX = (1 - sizeX) / 2.f;
     }
     worldView.setViewport(sf::FloatRect(posX, posY, sizeX, sizeY));
     // worldView.setCenter(entList[0].getPosition());
     mWindow.setView(worldView);
 
-
     // std::cout << "wS: "<<size.x << "x" << size.y << std::endl;
-
 }
 
-void Game::initECS(){
+void Game::initECS() {
     EntityFactory eFac;
-    playerID = eFac.createPlayer(registry,textures);
-    eFac.createBrute(registry,textures, {450.f,400.f}, {});
-    eFac.createGun(registry,textures, {500.f,400.f}, 0);
-    eFac.createHench(registry,textures, {550.f,400.f}, {});
-    eFac.createBoss(registry,textures, {600.f,400.f}, {});
+    playerID = eFac.createPlayer(registry, textures);
+    eFac.createBrute(registry, textures, {450.f, 400.f}, {});
+    eFac.createGun(registry, textures, {500.f, 400.f}, 0);
+    eFac.createHench(registry, textures, {550.f, 400.f}, {});
+    eFac.createBoss(registry, textures, {600.f, 400.f}, {});
 
-    eFac.createBlock(registry,textures, {650.f,400.f});
-    eFac.createPlatform(registry,textures, {700.f,400.f});
-    eFac.createDoor(registry,textures, {750.f,400.f});
-    eFac.createUI(registry,textures, {800.f,200.f}, 0);
-    eFac.createLadder(registry,textures, {850.f,400.f});
+    eFac.createBlock(registry, textures, {650.f, 400.f});
+    eFac.createPlatform(registry, textures, {700.f, 400.f});
+    eFac.createDoor(registry, textures, {750.f, 400.f});
+    eFac.createUI(registry, textures, {800.f, 200.f}, 0);
+    eFac.createLadder(registry, textures, {850.f, 400.f});
     std::cout << "Boof?" << std::endl;
-    std::cout << "B: "<< registry.size() << std::endl;
+    std::cout << "B: " << registry.size() << std::endl;
 }
 
-
-void Game::run(){
+void Game::run() {
     initWindow();
     initTextures(entList);
     initECS();
-    entList[0].setOrigin(entList[0].getBoundingBox().width/2,
-                        entList[0].getBoundingBox().height/2);
+    entList[0].setOrigin(entList[0].getBoundingBox().width / 2,
+                         entList[0].getBoundingBox().height / 2);
 
     Entity o(textures.get(Textures::Landscape), -600.f, -200.f, false);
     o.scale(0.5f, 0.5f);
 
-
     sf::Clock clock;
     sf::Time timeSinceLastUpdate = sf::Time::Zero;
-    while(mWindow.isOpen()){
+    while (mWindow.isOpen()) {
         // sf::Time deltaTime = clock.restart();
         processEvents();
         timeSinceLastUpdate += clock.restart();
-        while(timeSinceLastUpdate > TimePerFrame){
+        while (timeSinceLastUpdate > TimePerFrame) {
             timeSinceLastUpdate -= TimePerFrame;
             processEvents();
             update(TimePerFrame);
@@ -160,49 +153,49 @@ void Game::run(){
             mWindow.draw(spri);
         }
 
-
-        // for(std::size_t i=0; i<entList.size(); ++i) {   
+        // for(std::size_t i=0; i<entList.size(); ++i) {
         //     entList[i].draw(mWindow);
         // }
         mWindow.display();
     }
 }
 
-
-void Game::update(sf::Time deltaTime){
-    if(playerOnRoof){
-            playerJump = -50;
-        if(playerOnFloor){
+void Game::update(sf::Time deltaTime) {
+    if (playerOnRoof) {
+        playerJump = -50;
+        if (playerOnFloor) {
             playerJump = -1;
         }
     }
-    if(playerJump>-400){
-        playerJump-=40;
-    }else{
-        playerJump=-0;
+    if (playerJump > -400) {
+        playerJump -= 40;
+    } else {
+        playerJump = -0;
     }
- 
-    float playerGrav = 20.f;
 
-    sf::Vector2f movement(0.f, (-1*playerJump)+playerGrav);
+    float playerGrav = 270.f;
 
-    playerOnFloor = false;
+    sf::Vector2f movement(0.f, (-1 * playerJump) + playerGrav);
+
+    playerOnFloor = true;
     playerOnRoof = false;
 
-    if(misMovingUp){
+    if (misMovingUp) {
         // movement.y -= 470.f;
-    }if(misMovingDown){
+    }
+    if (misMovingDown) {
         // movement.y += 200.f;
-    }if(misMovingLeft){
+    }
+    if (misMovingLeft) {
         movement.x -= 370.f;
-        if(entList[0].getFacing()){
+        if (entList[0].getFacing()) {
             entList[0].scale(-1.f, 1.f);
             entList[0].setFacing(false);
         }
-        
-    }if(misMovingRight){
+    }
+    if (misMovingRight) {
         movement.x += 370.f;
-        if(!entList[0].getFacing()){
+        if (!entList[0].getFacing()) {
             entList[0].scale(-1.f, 1.f);
             entList[0].setFacing(true);
         }
@@ -212,40 +205,57 @@ void Game::update(sf::Time deltaTime){
     const auto view = registry.view<Draw, Movement>();
     const auto view2 = registry.view<Draw, Physics>();
     for (const entt::entity e : view) {
-        sf::Sprite spri = view.get<Draw>(e).sprite;
-        sf::FloatRect eBounds = spri.getGlobalBounds();
+        float xIdealCoordinate = 0;
+        float yIdealCoordinate = 0;
+        bool movex = true;
+        bool movey = true;
+        sf::Sprite spriX = view.get<Draw>(e).sprite;
+        sf::Sprite spriY = view.get<Draw>(e).sprite;
+        sf::Vector2f vel = view.get<Movement>(e).velocity * deltaTime.asSeconds();
+        sf::Vector2f velx = {vel.x, 0.f};
+        sf::Vector2f vely = {0.f, vel.y};
+        spriX.move(velx);
+        spriY.move(vely);
+        // spri.move(velx);
+
+        sf::FloatRect eXBounds = spriX.getGlobalBounds();
+        sf::FloatRect eYBounds = spriY.getGlobalBounds();
 
         for (const entt::entity j : view2) {
             sf::FloatRect jBounds = view2.get<Draw>(j).sprite.getGlobalBounds();
-            // std::cout << "--- "<< jBounds.left <<" --- " << eBounds.top << "--- " << jBounds.intersects(eBounds) << std::endl;
-            
-             if(jBounds.height != eBounds.height){
-                //  std::cout << "NINT NINT NINT" << std::endl;
-                if(jBounds.intersects(eBounds)){
-                    sf::Vector2f velZero = {0.f,0.f};
-                    std::cout << "INT INT INT" << std::endl;
-                    view.get<Movement>(e).velocity = velZero;
+            // //TODO find better way of checking if they are not the same object
+            if (jBounds.height != eXBounds.height) {
+                if (jBounds.intersects(eXBounds)) {
+                    if(!(eXBounds.left<=jBounds.left+jBounds.width && !(jBounds.left<=eXBounds.left))){
+                        std::cout << "IntRight" << std::endl;
+                        xIdealCoordinate = jBounds.left+jBounds.width;
+                    }else{
+                        std::cout << "IntLeft" << std::endl;
+                        xIdealCoordinate = jBounds.left - eXBounds.width;
+                    }   
+                    velx = {0.f, 0.f};
                 }
-             }
-
+                if (jBounds.intersects(eYBounds)) {
+                    // float yOvershoot = (jBounds.top-eYBounds.top+eYBounds.height);
+                    if(!(eYBounds.top<=jBounds.top+jBounds.height && !(jBounds.top<=eYBounds.top))){
+                        std::cout << "IntTop" << std::endl;
+                        yIdealCoordinate = jBounds.top+jBounds.height;
+                    }else{
+                        std::cout << "IntBot" << std::endl;
+                        yIdealCoordinate = jBounds.top - eYBounds.height;
+                    }
+                    vely = {0.f,0.f};
+                }
+            }
         }
-
-
-
-
-        // Moving the entity according to their velocity
-        spri.move(view.get<Movement>(e).velocity * deltaTime.asSeconds());
-        view.get<Draw>(e).sprite = spri;
+        sf::Sprite sprit = view.get<Draw>(e).sprite;
+        sprit.move(velx+vely);
+        if (yIdealCoordinate != 0){ sprit.setPosition(sprit.getPosition().x, yIdealCoordinate); }
+        if (xIdealCoordinate != 0){ sprit.setPosition(xIdealCoordinate, sprit.getPosition().y); }
+        view.get<Draw>(e).sprite = sprit;
     }
 
-
-    
-
-
     //  .setVelocity(movement * deltaTime.asSeconds());
-    
-
-
 
     // for(std::size_t i=0; i<entList.size(); ++i) {
     //     std::vector<Entity> intersects;
@@ -262,7 +272,7 @@ void Game::update(sf::Time deltaTime){
     //             // std::cout << "intersects:" << i << " " << j << std::endl;
     //             c = false;
     //             intersects.push_back(entList[j]);
-    //         } 
+    //         }
     //     }
     //     if(!c){
     //         float overshoot = 0;
@@ -278,7 +288,7 @@ void Game::update(sf::Time deltaTime){
     //                 if(i==0){
     //                     playerOnFloor = true;
     //                 }
-    //                 // std::cout << "entVol.y: " << entVol.y << " | entBox.bot: " 
+    //                 // std::cout << "entVol.y: " << entVol.y << " | entBox.bot: "
     //                 // << entBox.top + entBox.height << " | intBox.top: " << intBox.top;
     //                 overshoot = (intBox.top-(entBox.top+entBox.height));
     //                 // std::cout << " | overshoot: " << overshoot << std::endl;
@@ -300,15 +310,14 @@ void Game::update(sf::Time deltaTime){
     //                 }
     //                 yMult = -1.f;
     //             }
-                
+
     //         }
-            
+
     //         // entList[i].setVelocity(0, overshoot);
     //         // entList[i].move();
     //         entList[i].setVelocity(entVol.x*xMult, (entVol.y*yMult)+overshoot);
     //         entList[i].move();
     //     }
-
 
     //     // if(!c){
     //     //     bool v = true;
@@ -324,9 +333,9 @@ void Game::update(sf::Time deltaTime){
     //     //                 }else{
     //     //                     playerOnRoof = true;
     //     //                 }
-                            
+
     //     //                }
-                    
+
     //     //         }
     //     //     }
     //     //     if(!v){
@@ -344,6 +353,6 @@ void Game::update(sf::Time deltaTime){
     //     //         }
     //     //     }
     //     // }
-        
+
     // }
 }
